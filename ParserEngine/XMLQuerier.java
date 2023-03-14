4ff6d019815972a19939ea52fec53db239b1d462
Maxime Emonnot <61348720+MaximeEmonnot@users.noreply.github.com>
2023-03-14 12:13:22 +0100

    Ajout du XQuery (peut-être)

--- ParserEngine/XMLQuerier.java
+++ ParserEngine/XMLQuerier.java
@@ -3,9 +3,12 @@ package ParserEngine;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
+import javax.xml.xquery.*;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+
+import oracle.xml.xquery.OXQDataSource;
 
 public class XMLQuerier {
     
@@ -13,6 +16,13 @@ public class XMLQuerier {
 
     public NodeList SimpleQuery(Document document, String expression) throws Exception {
        return (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
+    }
+
+    public XQSequence ComplexQuery(Document document, String query) throws Exception {
+        OXQDataSource ds = new OXQDataSource();
+        XQConnection conn = ds.getConnection();
+        XQPreparedExpression expr = conn.prepareExpression(query);
+        return expr.executeQuery();
     }
 
     private static XPath xPath = XPathFactory.newInstance().newXPath();
